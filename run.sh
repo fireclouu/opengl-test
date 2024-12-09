06135613e856466018adc65f3ecca6d130fb70af
fireclouu <frcl@frcl.online>
2024-12-09 21:28:34 +0800

    Shell: Add navigation menu
    Fixed CMake and Makefile parameters for directories

--- run.sh
+++ run.sh
@@ -1,7 +1,63 @@
-#!/system/bin/sh
-BUILD_NAME="opengltest"
+#!/bin/bash
+exec_array=()
+opt=1
+BUILD_DIR=build
+BIN_DIR=$BUILD_DIR/bin
+PARENT_DIR=$PWD
 
-mkdir build 2> /dev/null && rm $BUILD_NAME 2> /dev/null
-cd build
-cmake ..
-make && ./$BUILD_NAME
+mkdir -p $BIN_DIR
+
+echo -e "\e[033mRun CMake...\e[0m"
+cmake -S . -B $BUILD_DIR > /dev/null
+if [ $? -ne 0 ]; then exit 1; fi
+echo -e "\e[033mRun Makefile...\e[0m"
+make -C $BUILD_DIR > /dev/null
+if [ $? -ne 0 ]; then exit 1; fi
+echo -e "\e[032mBuilds OK!\e[0m"
+
+for filename in $BIN_DIR/*; do
+  if [ -x $filename ] && [ -f $filename ]; then
+    exec_array+=("$filename")
+  fi
+done
+
+while [ -z "$opt" ] || [ "$opt" -ne 0 ]; do
+  echo
+  echo "Choose program to run: "
+  for ((i = ${#exec_array[@]} - 1; i >=0; i--)); do
+    echo -ne "\e[033m$((${#exec_array[@]}-${i})): \e[0m"
+    echo "${exec_array[$i]}" | awk -F/ '{print $3}'
+  done
+  echo -e "\e[033m0: \e[0mExit"
+  echo
+
+  read -p "Your choice: " opt
+  if [ -z "$opt" ] || [ "$opt" -lt 0 ] || [ "$opt" -gt ${#exec_array[@]} ]; then
+    echo -e "\e[031mInvalid choice!\e[0m"
+  elif [ "$opt" -ne 0 ]; then
+    # inverted. reason is that so the newer exercises will appear on top
+    selected=$((${#exec_array[@]}-$opt))
+    echo -ne "\e[032mSelected "
+    echo -n "${exec_array[$selected]}" | awk -F/ '{print $3}'
+    echo -e "\e[0m"
+
+    echo "Current Directory will set to $PARENT_DIR/$BIN_DIR"
+    cd $PARENT_DIR/$BIN_DIR
+
+    echo -e "\e[033mTransferring to Program...\e[0m"
+    $PARENT_DIR/${exec_array[$selected]}
+    STATUS=$?
+    if [ $STATUS -eq 0 ]; then
+      echo -e "\e[032mProgram exited without errors. "
+    else
+      echo -e "\e[031mProgram exited with failure ($STATUS). "
+    fi
+    echo -ne "\e[0m"
+    echo -e "\e[033mTransferring back to shell...\e[0m"
+
+    echo "Current Directory will set to $PARENT_DIR"
+    cd $PARENT_DIR
+  fi
+done
+
+echo -e "\e[033mGood bye.\e[0m"
