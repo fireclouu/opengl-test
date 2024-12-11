812b89ce73fd0796959383b65348482e203bbbe0
fireclouu <imfireclouu@gmail.com>
2024-12-11 19:54:47 +0800

    Shell: user input handling improvements

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ for filename in $BIN_DIR/*; do
   fi
 done
 
-while [ -z "$opt" ] || [ "$opt" -ne 0 ]; do
+while true; do
   echo
   echo "Choose program to run: "
   for ((i = ${#exec_array[@]} - 1; i >=0; i--)); do
@@ -32,31 +32,43 @@ while [ -z "$opt" ] || [ "$opt" -ne 0 ]; do
   echo
 
   read -p "Your choice: " opt
-  if [ -z "$opt" ] || [ "$opt" -lt 0 ] || [ "$opt" -gt ${#exec_array[@]} ]; then
+  if ! [[ "$opt" =~ ^[0-9]+$ ]]; then
     echo -e "\e[031mInvalid choice!\e[0m"
-  elif [ "$opt" -ne 0 ]; then
-    # inverted. reason is that so the newer exercises will appear on top
-    selected=$((${#exec_array[@]}-$opt))
-    echo -ne "\e[032mSelected "
-    echo -n "${exec_array[$selected]}" | awk -F/ '{print $3}'
-    echo -e "\e[0m"
-
-    echo "Current Directory will set to $PARENT_DIR/$BIN_DIR"
-    cd $PARENT_DIR/$BIN_DIR
-    echo -e "\e[033mTransferring to Program...\e[0m"
-    $PARENT_DIR/${exec_array[$selected]}
-    STATUS=$?
-    if [ $STATUS -eq 0 ]; then
-      echo -e "\e[032mProgram exited without errors. "
-    else
-      echo -e "\e[031mProgram exited with failure ($STATUS). "
-    fi
-    echo -ne "\e[0m"
-    echo -e "\e[033mTransferring back to shell...\e[0m"
-
-    echo "Current Directory will set to $PARENT_DIR"
-    cd $PARENT_DIR
+    continue
   fi
+
+  if [[ "$opt" -eq 0 ]]; then
+    break
+  fi
+
+  if [[ "$opt" -gt ${#exec_array[@]} ]]; then
+    echo -e "\e[031mInvalid choice!\e[0m"
+    continue
+  fi
+
+  # inverted sort so the newer exercises will appear on first choice
+  selected=$((${#exec_array[@]}-$opt))
+  echo -ne "\e[032mSelected "
+  echo -n "${exec_array[$selected]}" | awk -F/ '{print $3}'
+  echo -e "\e[0m"
+
+  echo "Current Directory will set to $PARENT_DIR/$BIN_DIR"
+  cd $PARENT_DIR/$BIN_DIR
+
+  echo -e "\e[033mTransferring to Program...\e[0m"
+  eval "$PARENT_DIR/${exec_array[$selected]}"
+  STATUS=$?
+  echo -e "\e[033mTransferring back to shell...\e[0m"
+
+  if [ $STATUS -eq 0 ]; then
+    echo -e "\e[032mProgram exited without errors. "
+  else
+    echo -e "\e[031mProgram exited with failure ($STATUS). "
+  fi
+  echo -ne "\e[0m"
+
+  echo "Current Directory will set to $PARENT_DIR"
+  cd $PARENT_DIR
 done
 
 echo -e "\e[033mGood bye.\e[0m"
