326b6ad5605ef18a8c07059cbb84f917ed3eeedf
fireclouu <frcl@frcl.online>
2024-12-19 09:09:45 +0800

    Added uniform offsets x position of triangle exercise

---
+++ src/shaders/exercise-02-05-shader-vertex.glsl
@@ -0,0 +1,10 @@
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+out vec4 vertexColor;
+uniform float xOffset;
+
+void main() {
+  gl_Position = vec4(aPos.x + xOffset, aPos.yz, 1.0);
+  vertexColor = vec4(0.0, 0.28, 0.56, 1.0);
+}
